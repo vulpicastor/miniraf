05653a8138098ac49e19fb43cc4fca890102b2ce
Lizhou Sha <slz@mit.edu>
2016-11-18 14:18:55 -0500

    Change option average to mean in combine for less typing
    Signed-off-by: Lizhou Sha <slz@mit.edu>

--- miniraf/combine.py
+++ miniraf/combine.py
@@ -5,17 +5,17 @@ import sys
 from .util import stack_fits_data
 
 METHOD_MAP = {"median": lambda x: np.median(x, axis=0, overwrite_input=True),
-              "average": lambda x: np.mean(x, axis=0),
+              "mean": lambda x: np.mean(x, axis=0),
               "sum": lambda x: np.sum(x, axis=0)}
 
 SCALE_MAP = {"median": lambda a: a / np.median(a),
-             "average": lambda a: a / np.mean(a),
+             "mean": lambda a: a / np.mean(a),
             }
 
 def create_parser(subparsers):
     parser_combine = subparsers.add_parser("combine", help="combine help")
-    parser_combine.add_argument("-m", "--method", choices=["median", "average", "sum"], required=True)
-    parser_combine.add_argument("-s", "--scale", choices=["median", "average"])
+    parser_combine.add_argument("-m", "--method", choices=["median", "mean", "sum"], required=True)
+    parser_combine.add_argument("-s", "--scale", choices=["median", "mean"])
     parser_combine.add_argument("-o", "--output", metavar="OUTFILE", default=sys.stdout.buffer)
     parser_combine.add_argument("file", nargs="+")
     parser_combine.set_defaults(func=main)
